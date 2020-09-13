37922eb403148a28e9f0f23c64f26b8361460ab7
Alexandra Taft <taft.alexandra@gmail.com>
2020-09-13 15:56:36 -0400

    add bar chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,25 @@
 d3.json("data/samples.json").then((data) => {
     var nameIDs = data.names;
     var metadata = data.metadata;
-    var samples = data.samples;
-
+    var results = metadata.filter(speciman => speciman.id == sample);
+    var result = results[0];
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values; 
+    //create traces
+    var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        type: "bar",
+        name: otc_labels
+    };
+    //create data for plot
+    var data = [trace1];
+    //create layout attributes
+    var layout = {
+        title: "Bacteria Diversity",
+        xaxis: {title: "Participant IDs"},
+        yaxis: {title: "Values"}
+    }
+    Plotly.newPlot("bar", data, layout);
 }
