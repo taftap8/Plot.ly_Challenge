a65178e3d33225abf80713b340abf32d899c180c
Alexandra Taft <taft.alexandra@gmail.com>
2020-09-15 18:30:40 -0400

    drop down updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,25 +1,51 @@
-d3.json("data/samples.json").then((data) => {
-    var nameIDs = data.names;
-    var metadata = data.metadata;
-    var results = metadata.filter(speciman => speciman.id == sample);
-    var result = results[0];
-    var otu_ids = result.otu_ids;
-    var otu_labels = result.otu_labels;
-    var sample_values = result.sample_values; 
-    //create traces
-    var trace1 = {
-        x: otu_ids,
-        y: sample_values,
-        type: "bar",
-        name: otc_labels
-    };
-    //create data for plot
-    var data = [trace1];
-    //create layout attributes
-    var layout = {
-        title: "Bacteria Diversity",
-        xaxis: {title: "Participant IDs"},
-        yaxis: {title: "Values"}
-    }
-    Plotly.newPlot("bar", data, layout);
-}
+//fetch data and read JSON file
+d3.json("samples.json").then((data) => {
+    //select html
+    var dropDown = d3.select('#selDataset');
+    console.log(data);
+    //append to list
+    data.names.forEach((name) => {
+        dropDown.append('option').text(name).property('value', name);
+    })
+});
+
+// d3.json("samples.json").then((data) => {
+    
+//     console.log(data);
+//     var nameIDs = data.names;
+//     var metadata = data.metadata;
+//     var sample = data.samples;
+//     var sampleID = data.samples[0].id;
+//     console.log(sampleID);
+
+
+
+    // var otu_ids = result.otu_ids;
+    // var otu_labels = result.otu_labels;
+    // var sample_values = result.sample_values; 
+    
+    // //grab html values
+    // var dropDown = d3.select('#selDataset');
+    // for (index in data.names) {
+    //     dropDown.options[dropDown.options.length] = new Option(data.names[index], index);
+    // };
+//});
+    // function optionChange (value){
+    //     console.log(value);
+    // }
+    // //create traces
+    // var trace1 = {
+    //     x: otu_ids,
+    //     y: sample_values,
+    //     type: "bar",
+    //     name: otc_labels
+    // };
+    // //create data for plot
+    // var data = [trace1];
+    // //create layout attributes
+    // var layout = {
+    //     title: "Bacteria Diversity",
+    //     xaxis: {title: "Participant IDs"},
+    //     yaxis: {title: "Values"}
+    // };
+    // Plotly.newPlot("bar", data, layout);
